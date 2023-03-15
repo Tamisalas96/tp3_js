ae12e387a09dbc44203efd90eda01f8739a79ace
Tami Salas <salastamara3@gmail.com>
2023-03-15 17:18:26 -0300

    realice los ejercicios de arrays en la hoja de ejerciciosarrays y los dejé comentados

---
+++ ejerciciosarrays.js
@@ -0,0 +1,62 @@
+//let meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
+
+//document.write(meses)
+
+
+/*let continuar = true
+let primerasCiudades = []
+        
+        while(continuar){
+            let ciudades = prompt("Ingrese los nombres de paises/ciudades que desee añadir al arreglo")
+            if(ciudades ===""){
+                alert("Ud no ha ingresado nada ");
+                continuar = confirm("Desea continuar?");
+                continue;
+               
+            } if (ciudades === null ){
+                break;
+            } 
+            if (ciudades.charAt(0) !== ciudades.charAt(0).toUpperCase()) {
+                alert("Por favor, escriba el nombre con la primera letra en mayúscula.")
+                continue;
+            }
+            
+                primerasCiudades.push(ciudades);
+            
+        }
+            
+            if ( primerasCiudades.length > 0){
+                document.write("La longitud del arreglo resultante es: " + primerasCiudades.length+ "." + "<br><br>");
+                document.write("El primer elemento es " + primerasCiudades[0] + ", el tercer elemento es " + primerasCiudades[2] + ", y el ultimo elemento es " + primerasCiudades[primerasCiudades.length-1]+ "." + "<br><br>");
+                primerasCiudades.push("Paris");
+                document.write(primerasCiudades + "<br><br>")
+                document.write("El elemento que ocupa la segunda posicion es: " + primerasCiudades[1] + "<br><br>")
+                primerasCiudades[1] = "Barcelona";
+                document.write("El array resultante es: " + primerasCiudades + "." +  "<br><br>");
+                
+                alert("Programa terminado")
+            }else{
+                alert("Programa terminado");
+            }*/
+
+
+
+/*let cuenta = {};
+
+for (let i = 0; i < 50; i++) {
+  let dado1 = Math.floor(Math.random() * 6) + 1; 
+  console.log("dado 1: " + dado1)
+
+  let dado2 = Math.floor(Math.random() * 6) + 1; 
+  console.log("dado 2: " + dado2)
+
+  let suma = dado1 + dado2; 
+  console.log("la suma da: " + suma)
+  if (cuenta[suma]) {
+    cuenta[suma]++;              
+  } else {
+    cuenta[suma] = 1;
+  }
+}
+
+console.log(cuenta);*/
