88e863375bdf9063ecbcc9dd2dbf55043996519b
Tami Salas <salastamara3@gmail.com>
2023-03-16 00:43:42 -0300

    realice los ejercicios de funciones y los deje comentados

---
+++ ejerciciosfunciones.js
@@ -0,0 +1,54 @@
+/*function ImparOPar(num){  
+    if(num % 2 === 0 ){
+        return("El numero " + num + " es par")
+    }else{
+        return("El numero " + num + " es impar")
+    }
+}
+
+console.log(ImparOPar(7));
+console.log(ImparOPar(8));*/
+
+
+
+/*function cadenaDeTexto(cadena){
+    let minuscula = true;
+    let mayuscula = true;
+        for (let i = 0; i < cadena.length; i++) {
+            if (cadena[i] !== cadena[i].toLowerCase()) {
+            minuscula = false;
+            }
+            if (cadena[i] !== cadena[i].toUpperCase()) {
+            mayuscula = false;
+            }
+        }
+
+        if (minuscula) {
+            return "La cadena está formada solo por minúsculas";
+        } else if (mayuscula) {
+            return "La cadena está formada solo por mayúsculas";
+        } else {
+            return "La cadena está formada por minúsculas y mayúsculas";
+        }
+    }
+    console.log(cadenaDeTexto("todo en minuscula"));*/
+
+
+
+
+ /*let lado1 = parseInt(prompt("Ingrese un lado del cuadrado"));
+let lado2 = parseInt(prompt("Ingrese otro lado del cuadrado"));
+     
+function perimetroCuadrado(a, b){
+        perimetro = 2*(a + b);
+        return("El perimetro del cuadrado es: " + perimetro);
+    }
+    console.log((perimetroCuadrado(a, b)));*/
+
+
+
+    /*let numero = prompt("ingrese un numero")
+
+for(i=0;i<=10;i++){
+    document.write(`${numero} x  ${i} = ${numero * i}<br></br>`)
+}*/
